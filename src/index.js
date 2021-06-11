095a0572bbca7f8778d56b3bddd00fbb00b48e7e
Ivanna Lyzlova <49069055+lyzlova@users.noreply.github.com>
2021-06-11 13:52:15 +0300

    added pagination

--- src/index.js
+++ src/index.js
@@ -4,20 +4,34 @@ const BASE_URL = "https://randomuser.me/api/";
 const words = ["пользователь", "пользователя", "пользователей"];
 let arrResultUsers = [];
 let filtredUsers = [];
-let searchValue = "";
-let selectedOptions = "";
-let selectedDefault = true;
+let sortedUsers = [];
+const filter = { searchValue: "", gender: [], age: [] };
+const sortParams = { selectedOptions: "4", perPage: 5 };
+let countUsers = null;
+let currentPage = 1; //текущий номер страницы
+let totalPages; //всего страниц
+let startSliceUsers = 0;
+let endSliceUsers = 5;
+let isSelectPerpage = false;
 
 const refs = {
   loadMore: document.querySelector("[data-load-more]"),
   usersContainer: document.querySelector("[data-users-container]"),
   user: document.querySelector("#user-template"),
   spiner: document.querySelector("[data-load-spiner]"),
-  statistic: document.querySelector("#statistics"),
-  usersFilter: document.querySelector(".users__filters"),
+  statisticTemplate: document.querySelector("#statistics"),
+  statisticContainer: document.querySelector(".statistics"),
+  filter: document.querySelector(".filter"),
   search: document.querySelector("[data-input-search]"),
-  sort: document.querySelector(".sort"),
+  content: document.querySelector(".users-content"),
   select: document.querySelector("#sort-control"),
+  allCheckbox: Array.from(document.querySelectorAll('input[type="checkbox"]')),
+  paginationList: document.querySelector(".pagination__list"),
+  startStep: document.querySelector("#first-step"),
+  endStep: document.querySelector("#last-step"),
+  prevStep: document.querySelector("#prev-step"),
+  nextStep: document.querySelector("#next-step"),
+  selectPerPage: document.querySelector("#view-control"),
 };
 
 function getRandomUser(max) {
@@ -29,12 +43,22 @@ refs.loadMore.addEventListener("click", onLoad);
 function onLoad(e) {
   refs.spiner.classList.remove("is-hidden");
   refs.search.value = "";
+  sortParams.selectedOptions = "";
+  sortParams.perPage = 5;
+  refs.selectPerPage.selectedIndex = 0;
+  refs.select.selectedIndex = 0;
+  currentPage = 1;
   clearCardesContainer();
   fetchUsers();
+  filterAccordion();
+  onSearch();
+  onSelect();
+  onSelectPerPage();
+  onCheckedFeature();
 }
 
 function getUsers(randomUsers) {
-  const url = `${BASE_URL}?results=${randomUsers}`;
+  const url = `${BASE_URL}?&results=${randomUsers}`;
 
   return fetch(url)
     .then((response) => response.json())
@@ -47,10 +71,10 @@ function fetchUsers() {
   getUsers(getRandomUser(101)).then((users) => {
     arrResultUsers = [...users];
     filtredUsers = [...users];
-    replaceMurkup(arrResultUsers);
+    sortedUsers = [...users];
+    replaceMurkup(arrResultUsers.slice(0, 5));
     getStatistics(arrResultUsers);
-    onSearch();
-    onSelect();
+    createPagination();
   });
 }
 
@@ -84,18 +108,18 @@ function makeUserCard({
   )}-${dateRegistered.getUTCFullYear()}`;
 
   const replacedUser = refs.user.innerHTML
-    .replaceAll(/{{hrefImg}}/gi, picture.large)
+    .replace(/{{hrefImg}}/gi, picture.large)
     .replace(/{{lastName}}/, name.last)
     .replace(/{{firstName}}/, name.first)
     .replace(/{{gender}}/, gender)
-    .replaceAll(/{{phone}}/gi, phone)
-    .replaceAll(/{{email}}/gi, email)
+    .replace(/{{phone}}/gi, phone)
+    .replace(/{{email}}/gi, email)
     .replace(/{{state}}/, location.state)
     .replace(/{{city}}/, location.city)
     .replace(/{{street}}/, location.street.name)
     .replace(/{{birthday}}/, dateBirthdayContent)
     .replace(/{{registered}}/, dateRegisteredContent);
-  refs.usersContainer.insertAdjacentHTML("afterbegin", replacedUser);
+  refs.usersContainer.insertAdjacentHTML("beforeend", replacedUser);
 }
 
 function validDate(value) {
@@ -118,13 +142,14 @@ function getStatistics(users) {
       {}
     );
 
-  const replacesStatistics = refs.statistic.innerHTML
+  const replacesStatistics = refs.statisticTemplate.innerHTML
     .replace(/{{users}}/, users.length)
     .replace(/{{female}}/, female.length)
     .replace(/{{male}}/, male.length)
     .replace(/{{dominantGender}}/, dominantGender)
     .replace(/{{nationality}}/, createNationalityList(stats));
-  refs.usersContainer.insertAdjacentHTML("beforeend", replacesStatistics);
+
+  refs.statisticContainer.innerHTML = replacesStatistics;
 }
 
 function createNationalityList(stats) {
@@ -149,23 +174,24 @@ function clearCardesContainer() {
   refs.usersContainer.innerHTML = "";
 }
 
-function redrawing() {
+function redrawPage() {
   clearCardesContainer();
   replaceMurkup(filtredUsers);
   getStatistics(filtredUsers);
 }
 
 // Stage 2
-
-// filters
+// поиск на странице
 
 function onSearch() {
-  refs.usersFilter.classList.remove("is-hidden");
+  refs.filter.classList.remove("is-hidden");
 
   refs.search.addEventListener("input", (e) => {
-    searchValue = e.target.value.trim().toLowerCase();
+    filter.searchValue = e.target.value.trim().toLowerCase();
 
-    filtersBySearch(searchValue);
+    filterProducts();
+    redrawPage();
+    updatePagination();
   });
 
   refs.search.addEventListener("keypress", (e) => {
@@ -173,68 +199,283 @@ function onSearch() {
   });
 }
 
-function filtersBySearch(val) {
-  filtredUsers = arrResultUsers.filter((item) => {
-    const includesElemenet =
-      item.email.toLowerCase().includes(val) ||
-      item.phone.toLowerCase().includes(val) ||
-      item.name.first.toLowerCase().includes(val) ||
-      item.name.last.toLowerCase().includes(val);
-
-    return includesElemenet;
-  });
-
-  selectedDefault && redrawing();
-}
-
 // sort
 
 function onSelect() {
-  refs.sort.classList.remove("is-hidden");
+  refs.content.classList.remove("is-hidden");
 
   refs.select.addEventListener("change", function (e) {
-    filtersBySearch(searchValue);
-    selectedOptions = this.value;
-    rewdrowSelectResult();
+    sortParams.selectedOptions = this.value;
+
+    filterProducts();
+    redrawPage();
   });
 }
 
-function rewdrowSelectResult() {
-  if (selectedDefault) {
-    filtredUsers = filtredUsers.sort(sortByOption(selectedOptions));
-  }
+// фильтр по checked
 
-  redrawing();
+function onCheckedFeature() {
+  refs.allCheckbox.forEach((checkbox) => {
+    checkbox.addEventListener("click", (e) => {
+      let arr = e.target.name;
+      let value = e.target.value;
+
+      !filter[arr].includes(value)
+        ? filter[arr].push(value)
+        : (filter[arr] = filter[arr].filter((item) => item !== value));
+
+      filterProducts();
+      clearCardesContainer();
+      replaceMurkup(filtredUsers);
+      getStatistics(filtredUsers);
+      updatePagination();
+    });
+  });
 }
 
+// фильтрация
+
+function filterProducts() {
+  filtredUsers = arrResultUsers
+    .filter((item) => filtersBySearch(item, filter.searchValue))
+    .filter((item) => applyCheckedFields(item.gender, filter.gender))
+    .filter((item) => applyCheckedAgeFields(item.dob.age, filter.age))
+    .sort(sortByOption(sortParams.selectedOptions));
+
+  sortedUsers = [...filtredUsers];
+  filtredUsers = filtredUsers.slice(startSliceUsers, endSliceUsers);
+}
+
+const optionsSort = {
+  0: () => filtersBySearch(sortParams.selectedOptions),
+  1: (a, b) => a.name.last.localeCompare(b.name.last),
+  2: (a, b) => b.name.last.localeCompare(a.name.last),
+  3: (a, b) => b.gender.localeCompare(a.gender),
+  4: (a, b) => a.gender.localeCompare(b.gender),
+  5: (a, b) =>
+    b.gender.localeCompare(a.gender) || a.name.last.localeCompare(b.name.last),
+  6: (a, b) =>
+    b.gender.localeCompare(a.gender) || b.name.last.localeCompare(a.name.last),
+  7: (a, b) =>
+    a.gender.localeCompare(b.gender) || a.name.last.localeCompare(b.name.last),
+  8: (a, b) =>
+    a.gender.localeCompare(b.gender) || b.name.last.localeCompare(a.name.last),
+};
+
 function sortByOption(val) {
-  switch (val) {
-    case "1":
-      return (a, b) => b.name.last.localeCompare(a.name.last);
-    case "2":
-      return (a, b) => a.name.last.localeCompare(b.name.last);
-    case "3":
-      return (a, b) => a.gender.localeCompare(b.gender);
-    case "4":
-      return (a, b) => b.gender.localeCompare(a.gender);
-    case "5":
-      return (a, b) =>
-        a.gender.localeCompare(b.gender) ||
-        b.name.last.localeCompare(a.name.last);
-    case "6":
-      return (a, b) =>
-        a.gender.localeCompare(b.gender) ||
-        a.name.last.localeCompare(b.name.last);
-    case "7":
-      return (a, b) =>
-        b.gender.localeCompare(a.gender) ||
-        b.name.last.localeCompare(a.name.last);
-    case "8":
-      return (a, b) =>
-        b.gender.localeCompare(a.gender) ||
-        a.name.last.localeCompare(b.name.last);
-    default:
-      !selectedDefault;
-      filtersBySearch(searchValue);
+  if (sortParams.selectedOptions === "0") {
+    true;
+  } else {
+    return optionsSort[val];
+  }
+}
+
+// Stage 3
+
+function applyCheckedFields(arrParam, filterParam) {
+  return filterParam.length === 0 ? true : filterParam.includes(arrParam);
+}
+
+function filtersBySearch(item, filterParam) {
+  return filterParam === "" ? true : findIncludesElement(item, filterParam);
+}
+
+function findIncludesElement(item, val) {
+  const includesElemenet =
+    item.email.toLowerCase().includes(val) ||
+    item.phone.toLowerCase().includes(val) ||
+    item.name.first.toLowerCase().includes(val) ||
+    item.name.last.toLowerCase().includes(val);
+  return includesElemenet;
+}
+
+const ranges = {
+  "0-35": { min: 0, max: 35 },
+  "35-40": { min: 35, max: 40 },
+  "40-45": { min: 40, max: 45 },
+  "45+": { min: 45, max: Infinity },
+};
+
+function applyCheckedAgeFields(age, rangeParam) {
+  let range = Object.keys(ranges).find(
+    (range) => ranges[range].min <= age && ranges[range].max >= age
+  );
+
+  return rangeParam.length === 0 ? true : rangeParam.includes(range);
+}
+
+function onSelectPerPage() {
+  refs.selectPerPage.addEventListener("change", function (e) {
+    !isSelectPerpage;
+    if (this.value === "100") {
+      sortParams.perPage = sortedUsers.length;
+    } else {
+      sortParams.perPage = +this.value;
+    }
+
+    totalPages = Math.ceil(sortedUsers.length / sortParams.perPage);
+
+    cutUsers();
+    filterProducts();
+    redrawPage();
+    updatePagination();
+  });
+}
+
+// pagination
+
+function createPagination() {
+  totalPages = Math.ceil(filtredUsers.length / sortParams.perPage); //кол-во страниц
+  refs.paginationList.innerHTML = "";
+  addPaginationButtons();
+  onStepPagination();
+}
+
+function updatePagination() {
+  totalPages = Math.ceil(sortedUsers.length / sortParams.perPage); //кол-во страниц
+
+  refs.paginationList.innerHTML = "";
+  addPaginationButtons();
+  onStepPagination();
+}
+
+function createPagePaginationMarkUp(content, cls = "") {
+  return `<a href="#" class="pagination__item ${cls}" data-page="${content}">${content}</a>`;
+}
+
+function pageNumber(totalPages, maxPageVisible, currentPage) {
+  let half = Math.round(maxPageVisible / 2);
+  let to = maxPageVisible;
+  let total = totalPages;
+  let maxPages = maxPageVisible;
+
+  if (totalPages < 5) {
+    maxPages = totalPages;
+    half = 2;
+  }
+
+  if (currentPage + half >= total) {
+    to = total;
+  } else if (currentPage > half) {
+    to = currentPage + half;
+  } else if (currentPage + half < total) {
+    to = maxPages;
+  }
+  let from = to - maxPages;
+
+  return Array.from({ length: maxPages }, (_, i) => i + 1 + from);
+}
+
+function changeStylePagination() {
+  const currentActiveLink = document.querySelector(".pagination__item.active");
+
+  currentActiveLink && currentActiveLink.classList.remove("active");
+
+  if (currentPage > 1) {
+    refs.startStep.classList.remove("pagination-state--disabled");
+    refs.prevStep.classList.remove("pagination-state--disabled");
+  } else {
+    refs.startStep.classList.add("pagination-state--disabled");
+    refs.prevStep.classList.add("pagination-state--disabled");
+  }
+
+  if (+currentPage === totalPages) {
+    refs.nextStep.classList.add("pagination-state--disabled");
+    refs.endStep.classList.add("pagination-state--disabled");
+  } else {
+    refs.nextStep.classList.remove("pagination-state--disabled");
+    refs.endStep.classList.remove("pagination-state--disabled");
+  }
+}
+
+function onStepPagination() {
+  const paginationList = document.querySelector(".pagination__list");
+
+  paginationList.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target.nodeName === "A") {
+      currentPage = +e.target.textContent;
+    }
+
+    cutUsers();
+    filterProducts();
+    redrawPage();
+    addPaginationButtons();
+  });
+
+  refs.nextStep.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentPage += 1;
+    addPaginationButtons();
+
+    cutUsers();
+    filterProducts();
+    redrawPage();
+  });
+
+  refs.prevStep.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentPage -= 1;
+    addPaginationButtons();
+    cutUsers();
+    filterProducts();
+    redrawPage();
+  });
+
+  refs.startStep.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentPage = 1;
+    addPaginationButtons();
+
+    cutUsers();
+    filterProducts();
+    redrawPage();
+  });
+
+  refs.endStep.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentPage = totalPages;
+    addPaginationButtons();
+
+    cutUsers();
+    filterProducts();
+    redrawPage();
+  });
+}
+
+function addPaginationButtons() {
+  refs.paginationList.innerHTML = "";
+  changeStylePagination();
+
+  let pages = pageNumber(totalPages, 5, currentPage);
+
+  pages.forEach((pageNumber) => {
+    const isCurrentPage = currentPage === pageNumber;
+    const button = createPagePaginationMarkUp(
+      pageNumber,
+      isCurrentPage ? "active" : "",
+      false
+    );
+    refs.paginationList.insertAdjacentHTML("beforeend", button);
+  });
+}
+
+function cutUsers() {
+  startSliceUsers = (currentPage - 1) * sortParams.perPage;
+  endSliceUsers = startSliceUsers + sortParams.perPage;
+}
+
+function filterAccordion() {
+  const dropdownFilter = document.querySelectorAll("[data-select]");
+
+  if (dropdownFilter) {
+    dropdownFilter.forEach((elem) => {
+      elem.addEventListener("click", changePath);
+    });
+  }
+
+  function changePath(e) {
+    this.querySelector("[data-filter-path]").classList.toggle("is-open");
+    this.nextElementSibling.classList.toggle("visually-hidden");
   }
 }
